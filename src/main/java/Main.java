490bdf9d92076562907ca82badfae59233616577
Margarin <margarin@student.21-school.ru>
2024-11-25 19:05:28 +0300

    add binarysearch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import model.Model;
 import model.impl.Bus;
 import model.impl.Student;
 import model.impl.User;
+import service.Search;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +18,15 @@ public class Main {
         System.out.println(student);
         User user = User.builder().email("a@mail.com").name("Login").pass("qwerty12345").build();
         System.out.println(user);
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(Student.builder().gpa(7.5).group("AZ-00").number(15282).build());
+        studentList.add(Student.builder().gpa(7.5).group("AZ-00").number(15283).build());
+        studentList.add(Student.builder().gpa(7.5).group("AZ-00").number(15292).build());
+        studentList.add(Student.builder().gpa(7.5).group("AZ-00").number(15382).build());
+        studentList.add(Student.builder().gpa(7.5).group("AZ-00").number(15782).build());
+        Student students = Student.builder().gpa(7.5).group("AZ-00").number(3).build();
+        System.out.println(Search.binarySearch(studentList,students, Comparator.comparingInt(Student::getNumber)));
+        students = studentList.get(3);
+        System.out.println(Search.binarySearch(studentList,students, Comparator.comparingInt(Student::getNumber)));
     }
 }
