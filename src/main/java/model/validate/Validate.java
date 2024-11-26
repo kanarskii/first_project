90480b2814d390d7c10218a5504ecb8103a921c6
Игорь Абзалов <9364@bk.ru>
2024-11-26 21:43:23 +0300

    Валидация закончена

--- src/main/java/model/validate/Validate.java
+++ src/main/java/model/validate/Validate.java
@@ -2,6 +2,9 @@ package model.validate;
 
 public interface Validate {
 
-    boolean validate(String str);
-       // str.matches(".+ \\d+")
+    boolean stringValidate(String str);
+
+    boolean intValidate(Integer number);
+
+    boolean doubleValidate(Double gpa);
 }
--- src/main/java/model/validate/ValidateMethods.java
+++ src/main/java/model/validate/ValidateMethods.java
@@ -3,7 +3,7 @@ package model.validate;
 public class ValidateMethods implements Validate{
 
     @Override
-    public boolean validate(String str) {
+    public boolean stringValidate(String str) {
         boolean result;
 
         if (str.contains("@")&& (str.contains("."))){
@@ -14,10 +14,27 @@ public class ValidateMethods implements Validate{
         }
         return result;
     }
+
+    @Override
+    public boolean intValidate(Integer number) {
+        if (number < 0) {
+            return false;
+        }
+        else return true;
+    }
+
+    @Override
+    public boolean doubleValidate(Double doubl) {
+        if (doubl < 0) {
+            return false;
+        }
+        else return true;
+    }
+
     public static boolean isEmailValid(String str) {
         return str.matches("^[\\w-\\.]+@[\\w-]+(\\.[\\w-]+)*\\.[a-z]{2,}$");
     }
     private boolean isWordValid(String str) {
-        return str.matches("^[A-Za-z-]*$");
+        return str.matches("^[A-Za-z-0-9]*$");
     }
 }
