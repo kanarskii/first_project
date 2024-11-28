a6db75c0fe5179525b0a39668a8f383c80fff8ad
Victor Drobysheusky <akreshios@gmail.com>
2024-11-28 16:26:16 +0300

    add new validate methods

--- src/main/java/model/validate/Validate.java
+++ src/main/java/model/validate/Validate.java
@@ -11,4 +11,15 @@ public interface Validate {
     boolean busValidate(String classType, String number,String model, Integer mileage);
     boolean userValidate(String classType, String name,String pass, String email);
     boolean studentValidate(String classType, String group,Double gpa, Integer number);
+
+
+    boolean isInteger(String str);
+    boolean isDouble (String str);
+    boolean isEmail(String str);
+    boolean isWord(String str);
+
+    boolean isUser(String name, String pass, String email);
+    boolean isBus(String number, String model, String mileage);
+    boolean isStudent(String group, String gpa, String number);
+
 }
