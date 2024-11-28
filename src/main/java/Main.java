134171400b67d71c2adedd3a850bacd39e3d0af7
Victor Drobysheusky <akreshios@gmail.com>
2024-11-28 18:45:54 +0300

    javaDoc main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 import ui.UI;
 import ui.UI_impl;
 
+/**
+ * Командный тестовый проект Aston 11.2024
+ * @autor Евгений Канарский, Виктор Дробышевский, Илья Баренков, Игорь Абзалов, Игорь Кашолкин
+ * @version 1.0
+ */
+
 public class Main {
     public static void main(String[] args) {
         UI ui = new UI_impl();
